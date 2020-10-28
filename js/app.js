910ea4b29ccad10e83e553f297a11d947b7a6251
tediko <tediko123@wp.pl>
2020-10-28 12:18:24 +0100

    Added circle progress bar

--- js/app.js
+++ js/app.js
@@ -15,3 +15,34 @@ const openMenu = () => {
 }
 
 nav.addEventListener('click', openMenu);
+
+/* CIRCLE PROGRESS BAR */
+const boxes = document.querySelectorAll('.info__box');
+boxes.forEach(box => {
+    const circle = box.querySelector('[data-circle]');
+    const count = box.querySelector('[data-count]');
+    const progress = parseFloat(count.dataset.count);
+    const initialProgressValue = parseFloat(window.getComputedStyle(circle).strokeDashoffset);
+    let progressInterval;
+
+    const maxSteps = 100;
+    let step = 0;
+    let stepValue = progress * (1 / 100);
+    let finalProgress = 0;
+    const delay = 3000 / maxSteps;
+    
+    progressInterval = setInterval(() => {
+        if(step < progress) {
+            step += stepValue;
+            count.innerHTML = parseInt(step);
+            if (progress > 100) {
+                finalProgress = 630 - (step * 630) / progress;
+            } else {
+                finalProgress = initialProgressValue - (initialProgressValue * step) / 100;
+            }
+            circle.style.strokeDashoffset = finalProgress;
+        } else {
+            clearInterval(progressInterval);
+        }
+    }, delay);
+})
